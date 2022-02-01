2e2f98afeff54d0cdcbfd4401319ce305fcc56b7
Dennis Meyer <snooops@dns-serve.de>
2022-02-01 13:12:51 +0100

    Update install-my-vimrc.sh
    added some plugins

--- install-my-vimrc.sh
+++ install-my-vimrc.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# installation of some plugins
+# identline
+git clone https://github.com/Yggdroot/indentLine.git ~/.vim/pack/vendor/start/indentLine
+vim -u NONE -c "helptags  ~/.vim/pack/vendor/start/indentLine/doc" -c "q"
+# ale
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+
 git clone https://github.com/snooops/vimrc ~/vimrc
 echo "source ~/vimrc/vimrc" >> ~/.vimrc
 
